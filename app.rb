893d1628c542a52984cbfd094b828a9e704f2d4f
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2019-02-09 18:35:00 +0900

    POSTできるまで

--- app.rb
+++ app.rb
@@ -2,11 +2,32 @@ require 'beebotte'
 require 'dotenv/load'
 
 def handle(message)
+  data = JSON.parse(message)['data'].first
+  room = data['room']
+  action = data['action']
 
+  mac_address = case room
+                when 'living'
+                  ENV['MAC_LIVING']
+                when 'bedroom'
+                  ENV['MAC_BEDROOM']
+                when 'office'
+                  ENV['MAC_OFFICE']
+                end
+  ip_address = rarp(mac_address)
+  if action == 'on'
+    send_data(ip_address, ENV['ON_MESSAGE'])
+  else
+    send_data(ip_address, ENV['OFF_MESSAGE'])
+  end
+end
+
+def arp
+  `arp -a`
 end
 
 def rarp(mac_address)
-
+  extract_ip_address(arp, mac_address)
 end
 
 def extract_ip_address(arp_response, mac_address)
@@ -18,7 +39,11 @@ def extract_ip_address(arp_response, mac_address)
 end
 
 def send_data(ip_address, message)
-
+  puts ip_address
+  puts message
+  uri = URI.parse("http://#{ip_address}/smart/")
+  response = Net::HTTP.post(uri, message)
+  puts response.body
 end
 
 if $0 == __FILE__
